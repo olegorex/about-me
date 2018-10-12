603f91a38470b2d3e2367adc658330a397a7de2a
ды <ls>
2018-10-12 14:51:57 +0600

    adding about me page

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  document.getElementById('copyright').addEventListener("click", function() {
+  this.style.color = 'purple';
+});
+  //$("#copyright").click(function(){
+  //  $(this).css('color','purple');
+  //});"
+});
